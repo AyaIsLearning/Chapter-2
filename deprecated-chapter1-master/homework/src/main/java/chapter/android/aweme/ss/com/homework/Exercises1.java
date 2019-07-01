a0aba329c21676c0d506b142c2c9e7445512915e
Aya <787406155@qq.com>
2019-07-01 18:20:24 +0800

    finish exersises 2

--- deprecated-chapter1-master/homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises1.java
+++ deprecated-chapter1-master/homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises1.java
@@ -3,6 +3,7 @@ package chapter.android.aweme.ss.com.homework;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * 作业1：
@@ -13,6 +14,15 @@ public class Exercises1 extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(Exercises1.this,"创建了exercises1",Toast.LENGTH_LONG).show();
+        System.out.println("创建了exercises1");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(Exercises1.this,"摧毁了exercises1",Toast.LENGTH_LONG).show();
+        System.out.println("摧毁了exercises1");
     }
 
 }
--- deprecated-chapter1-master/homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises2.java
+++ deprecated-chapter1-master/homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises2.java
@@ -4,22 +4,86 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 /**
  * 作业2：一个抖音笔试题：统计页面所有view的个数
  * Tips：ViewGroup有两个API
  * {@link android.view.ViewGroup #getChildAt(int) #getChildCount()}
  * 用一个TextView展示出来
+ *
+ * 解答
+ * 设计的界面为一个线性布局，内部第一层为一个TextView 其下为一个有5个item的ListView
+ *
+ * 最终能在嘴上端的TextView中显示出 7 个
+ *
  */
 public class Exercises2 extends AppCompatActivity {
+
+
+    private String[] city = {"Beijing", "Nanjing", "Shanghai", "Chengdu", "Tianjin"};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Exercises2.this, android.R.layout.simple_list_item_1, city);
+
+        ListView list = findViewById(R.id.TestList);
+        list.setAdapter(adapter);
+
+        View current_view = getWindow().getDecorView().findViewById(R.id.root);
+        int viewCount=getViewCount(current_view);
+
+        TextView text=findViewById(R.id.TotalViewNum);
+        text.setText("共有"+viewCount+"个view");
+        Toast.makeText(Exercises2.this,"共有"+viewCount,Toast.LENGTH_LONG).show();
+        
+        /*
+        setContentView(R.layout.im_list_item);
+        View current_view = getWindow().getDecorView().findViewById(R.id.root2);
+        int viewCount=getViewCount(current_view);
+        Toast.makeText(Exercises2.this,"共有"+viewCount,Toast.LENGTH_LONG).show();
+         */
+        //System.out.println(viewCount);
+
     }
 
     public static int getViewCount(View view) {
         //todo 补全你的代码
-        return 0;
+        int viewCount = 0;
+        if (view == null) {
+            return 0;
+        }
+        if (view instanceof ViewGroup) {
+            viewCount++;
+
+            System.out.println(((ViewGroup) view).getChildCount());
+
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View temp_view = ((ViewGroup) view).getChildAt(i);
+
+                if (temp_view instanceof ViewGroup) {
+                    if(temp_view instanceof ListView){
+                        viewCount += ((ListView) temp_view).getCount();
+                    }
+                    else{
+                        viewCount += getViewCount(temp_view);
+                    }
+                }
+                else {
+                    viewCount++;
+                }
+            }
+        }
+        return viewCount;
     }
 }
